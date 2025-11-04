7bbcf7fe1abd3b6245a7c6f6420b820b282aafc7
anjali <anjali31017@gmail.com>
2025-11-04 21:50:55 +0530

    get update profile

--- backend/app/controller/user_controller.py
+++ backend/app/controller/user_controller.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from app.models.users import UserModel
 from beanie.operators import Or, Eq
 
@@ -21,3 +22,37 @@ class UserController:
             print(str(e))
             return None
     
+    async def _find_active_user(self, user_id: str) -> UserModel | None:
+        try:
+            obj_id = ObjectId(user_id)
+            return await UserModel.find_one(
+                UserModel.id == obj_id,
+                UserModel.is_deleted == False
+            )
+        except Exception as e:
+            print("Error finding user:", e)
+            return None
+    
+    
+    async def get_user(self, id: str) -> UserModel | None:
+        try:
+             return await self._find_active_user(id)
+        except Exception as e:
+            print(str(e))
+            return None
+    
+    async def update_user(self, user_id: str, update_data: dict) -> UserModel | None:
+        try:
+            user = await self._find_active_user(user_id)
+            if not user:
+                return None
+
+            for key, value in update_data.items():
+                setattr(user, key, value)
+
+            await user.save()
+            return user
+
+        except Exception as e:
+            print("Error updating user:", e)
+            return None
